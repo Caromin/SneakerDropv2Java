961f65aa7b5ba8a9c03bf0c7d886860390540b14
Caromin <carominucf@gmail.com>
2019-04-02 12:55:56 -0400

    able to change header class based on url

--- client/src/app/components/nav/nav.component.ts
+++ client/src/app/components/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-nav',
@@ -6,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  route: boolean;
 
-  constructor() { }
+  constructor(location: Location, router: Router) {
+    router.events.subscribe(val => {
+// tslint:disable-next-line: triple-equals
+      if (location.path() !== '') {
+        this.route = false;
+      } else {
+        this.route = true;
+      }
+    });
+  }
 
   ngOnInit() {
   }
