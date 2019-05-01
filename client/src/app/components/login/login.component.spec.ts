7a3b6a7fa7a336c149a22a3f1fac9360f7b153a0
Caromin <carominucf@gmail.com>
2019-05-01 11:35:40 -0400

    pause on jasmine testing

--- client/src/app/components/login/login.component.spec.ts
+++ client/src/app/components/login/login.component.spec.ts
@@ -1,31 +1,52 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
-import { Router } from '@angular/router';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { UsersService } from 'src/app/services/users.service';
+import { Observable, of } from 'rxjs';
 
-describe('LoginComponent', () => {
+// class MockService {
+//   checkForExistingUser(user) {
+//     return of({ username: 'meowmix', password: 'meowmix'});
+//   }
+// }
+
+fdescribe('LoginComponent', () => {
   let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+  let userService: UsersService;
+  let mockUser: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
       imports: [FormsModule, ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule],
-      providers: []
+      providers: [UsersService]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = TestBed.createComponent(LoginComponent).componentInstance;
+    userService = TestBed.get(UsersService);
+    spyOn(component, 'getUser').and.returnValue({ username: 'meowmix', password: 'meowmix'});
+    mockUser = { username: 'meowmix', password: 'meowmix'};
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('ngOnInit should set the user property to new instance of formBuilder', () => {
+    component.ngOnInit();
+
+    expect(component.getUser()).toEqual(mockUser);
+  });
+
+  fit('compare user should redirect to userpage on success or error message if failure', () => {
+    component.ngOnInit();
+    spyOn(userService, 'checkForExistingUser').and.returnValue(of({ username: 'meowmix', password: 'meowmix'}));
+    expect(component.compareUser()).toEqual(false);
   });
 });
--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { StorageService } from 'src/app/services/storage.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsersService } from 'src/app/services/users.service';
 import { Router } from '@angular/router';
+import { userInfo } from 'os';
 
 @Component({
   selector: 'app-login',
@@ -15,13 +15,28 @@ export class LoginComponent implements OnInit {
   private user: FormGroup;
   private message: string[];
 
+  getUser() {
+    return this.user;
+  }
+  setUser(user) {
+      this.user = user;
+  }
+
+  getMessage() {
+    return this.message;
+  }
+
+  setMessage(message) {
+    this.message = message;
+  }
+
   constructor(
-    private http: HttpClient,
     private storageService: StorageService,
     private formBuilder: FormBuilder,
     private userService: UsersService,
     private router: Router) { }
 
+  // error shows at the start, because there are validators onInit
   ngOnInit() {
     this.user = this.formBuilder.group({
       username: [null, [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
@@ -29,19 +44,30 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  compareUser() {
+  compareUser(): boolean {
     const user = this.user.value;
+    userInfo(user);
     this.userService.checkForExistingUser(user).subscribe(result => {
 
       if (result === null) {
         this.message = ['The username or the password was incorrect'];
         this.updateMessage(this.message);
+
+        return false;
       } else {
         this.message = [];
         this.updateMessage(this.message);
         this.router.navigateByUrl('/user');
+
+        return true;
       }
+    },
+    error => {
+      this.message = ['There is a problem with the server, please try again later.'];
+      this.updateMessage(this.message);
+      return false;
     });
+    return false;
   }
 
   updateMessage(messages: string[]) {
